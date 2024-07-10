0902fd136049bcf09135d3b3c07d8832fe7cd36b
haseebmohsin <haseeb11634@gmail.com>
2024-07-10 12:33:17 +0500

    GET request in nextjs...

---
+++ src/app/comments/data.ts
@@ -0,0 +1,22 @@
+export const comments = [
+  {
+    id: 1,
+    text: "This is the first comment.",
+  },
+  {
+    id: 2,
+    text: "This is the second comment.",
+  },
+  {
+    id: 3,
+    text: "This is the third comment.",
+  },
+  {
+    id: 4,
+    text: "This is the fourth comment.",
+  },
+  {
+    id: 5,
+    text: "This is the fifth comment.",
+  },
+];
