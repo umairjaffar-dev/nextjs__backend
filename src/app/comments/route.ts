61e5a39fe6a11707cb7b426c608db730c1ea090a
haseebmohsin <haseeb11634@gmail.com>
2024-07-10 12:55:31 +0500

    making post request with next js...

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,5 +1,21 @@
 import { comments } from "./data";
 
 export async function GET() {
-    return Response.json(comments)
+  return Response.json(comments);
+}
+
+export async function POST(request: Request) {
+  const comment = await request.json();
+  const data = {
+    id: comments.length + 1,
+    text: comment.text,
+  };
+  comments.push(data);
+  return new Response(JSON.stringify(data), {
+    headers: {
+      "Content-Type": "Application/json",
+    },
+    status: 201,
+    statusText:"added a new comment."
+  });
 }
